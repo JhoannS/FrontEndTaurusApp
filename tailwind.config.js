553596194017908b6df248cc2bd8ec6e9d7ad7be
JhoannS <sebastianzamudio2004@gmail.com>
2025-02-21 14:05:38 -0500

    Creación del login de la app, pendiente de diseñar imagen referente

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./*.html"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#1E3A8A",
+          secondary: "#9333EA",
+          accent: "#FACC15",
+          background: "#F3F4F6",
+          text: "#111827",
+        },
+      },
+    },
+    plugins: [],
+  };
+  

14f25528b48a9ef26ae563c26b9db6778bb2d60b
JhoannS <sebastianzamudio2004@gmail.com>
2025-02-21 12:44:26 -0500

    Instalacion de tipografias, iconos de google fonts, icons

--- Assets/js/styles.js
+++ Assets/js/styles.js
@@ -1,13 +1,51 @@
 tailwind.config = {
     theme: {
         extend: {
+            
             colors: {
-                primary: "#1E3A8A",
-                secondary: "#9333EA",
-                accent: "#FACC15",
-                background: "#F3F4F6",
-                text: "#111827",
+                mono: {
+                  blanco: '#F5EFFF',
+                  negro: '#060809',
+                },
+                bg: {
+                  empty: '#1F1D2B',
+                },
+                semaforo: {
+                  verde: '#1BCC75',
+                  amarillo: '#FFA823',
+                  rojo: '#FF3131',
+                },
+                secundary: {
+                  default: '#1A2130',
+                  light: '#A5B8D4',
+                },
+                machine: {
+                  primary: '#57CC99',
+                  secundary: '#C7F9CC',
+                  complement: '#80ED99'
+                },
+                guru: {
+                  primary: '#BB9457',
+                  secundary: '#FFE6A7',
+                  complement: '#99582A'
+                },
+                shopper: {
+                  primary: '#3D4AEE',
+                  secundary: '#87A2FF',
+                  complement: '#8E9AAF'
+                },
+                essentials: {
+                  primary: '#FF4D6D',
+                  secundary: '#FFB3C1',
+                  complement: '#FF758F'
+                },
+                smart: {
+                  primary: '#DEE2FF',
+                  secundary: '#FEEAFA',
+                  complement: '#EFD3D7'
+                },
             },
         },
     },
 };
+

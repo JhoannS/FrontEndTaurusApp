9a95db4484fa3d357e568382082f1dc6f77dfa5f
JhoannS <sebastianzamudio2004@gmail.com>
2025-02-26 09:27:25 -0500

    Actualizacion de formularios con funciones de buscador, diferencias entre estados y stock

--- Assets/js/items.js
+++ Assets/js/items.js
@@ -1,17 +1,71 @@
-function actualizarColores() {
+function stock() {
     let cantidades = document.querySelectorAll(".cantidad");
 
     cantidades.forEach(elemento => {
-        let valor = parseInt(elemento.textContent);
+        let valor = parseInt(elemento.textContent.trim().replace(/\D/g, "")); // Elimina todo excepto números
 
         if (valor <= 9) {
-            elemento.classList.add("bg-semaforo-rojo", "text-mono-blanco");
+            elemento.classList.add("bg-semaforo-rojo");
         } else if (valor >= 10 && valor <= 14) {
-            elemento.classList.add("bg-semaforo-amarillo", "text-mono-blanco");
+            elemento.classList.add("bg-semaforo-amarillo");
         } else if (valor >= 15) {
-            elemento.classList.add("bg-semaforo-verde", "text-mono-blanco");
+            elemento.classList.add("bg-semaforo-verde");
         }
     });
 }
 
-actualizarColores(); // Llamar la función al cargar la página
+function estado() {
+    let estados = document.querySelectorAll(".estado");
+
+    estados.forEach(actual => {
+        let estadoActual = actual.textContent.trim(); // Solo obtiene el texto
+
+        if (estadoActual === "Inactivo") {
+            actual.classList.add("bg-semaforo-rojo");
+        } else if (estadoActual === "Suspendido") {
+            actual.classList.add("bg-semaforo-amarillo");
+        } else if (estadoActual === "Activo") {
+            actual.classList.add("bg-semaforo-verde");
+        }
+    });
+}
+
+function buscarProducto() {
+    let input = document.getElementById("buscador").value.toLowerCase().trim();
+    let filas = document.querySelectorAll("#tabla tbody tr");
+    let mensajeNoResultados = document.getElementById("mensaje-no-resultados");
+    let hayCoincidencias = false;
+
+    filas.forEach(fila => {
+        let textoFila = fila.textContent.toLowerCase();
+
+        if (textoFila.includes(input)) {
+            fila.classList.remove("fade-out");
+            fila.classList.add("fade-in");
+            setTimeout(() => {
+                fila.style.display = "";
+            }, 200);
+            hayCoincidencias = true;
+        } else {
+            fila.classList.remove("fade-in");
+            fila.classList.add("fade-out");
+            setTimeout(() => {
+                fila.style.display = "none";
+            }, 200);
+        }
+    });
+
+    // Mostrar mensaje si no hay coincidencias
+    if (hayCoincidencias) {
+        mensajeNoResultados.classList.add("hidden");
+    } else {
+        mensajeNoResultados.classList.remove("hidden");
+    }
+};
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    stock();
+    estado();
+});
